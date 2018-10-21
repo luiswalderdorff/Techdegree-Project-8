d82f3353d9d952aebb76b4b49922c35e820d9043
Luis Waderdorff <walderdorff@live.de>
2018-10-21 11:44:07 +0200

    Problem selecting dynamic elements

--- js/script.js
+++ js/script.js
@@ -7,16 +7,20 @@ const randomOptions = {
 };
 
 const usersClass =  document.querySelector(".users");
-const modalClass= document.querySelector('.modal');
+const modalClass = document.querySelector('.modal');
+const userBox = document.querySelector(".user-box");
 
 // Fetch Request
+
+let userData;
 
 fetch(randomAPI)
   .then(response => response.json())
   .then(data => {
     users = data.results;
     displayUsers(users);
-});
+}).then(data => userData = data)
+  .then(data => console.log(userData));
 
 //Capitalize Words
 
@@ -38,26 +42,36 @@ function displayUsers(data) {
   userHTML += '</ul>';
   usersClass.innerHTML = userHTML;
 
-    // Hidden Modal Interface
-    var modalHTML = '<ul class="modal" id="mod"';
-    data.forEach(function(results) {
-      modalHTML += `<li class="modal-box">`;
-      modalHTML += '<img src="' + results.picture.large + '"></li>';
-      modalHTML += `<p>${results.name.first} ${results.name.last}</p>`;
-      modalHTML += `<p>${results.email}</p>`;
-      modalHTML += `<p>${results.cell}</p>`;
-      modalHTML += `<p>${results.location.street}, ${results.location.city}, ${results.location.state}, ${results.location.postcode}</p>`;
-      modalHTML += `<p>${results.dob.date.substring(0.10)}</p>`;
-    }) // end each
-    modalHTML += '</ul>';
-    modalClass.innerHTML = modalHTML;
+
   }; // end each
 
+  // Hidden Modal Interface: Try to create it when clicked. The one that is needed
+  // var modalHTML = '<ul class="modal" id="mod">';
+  // data.forEach(function(results) {
+  //   modalHTML += `<li class="modal-box">`;
+  //   modalHTML += '<img class="modal-pic" src="' + results.picture.large + '">';
+  //   modalHTML += `<p>${results.name.first} ${results.name.last}</p>`;
+  //   modalHTML += `<p>${results.email}</p>`;
+  //   modalHTML += `<p>${results.cell}</p>`;
+  //   modalHTML += `<p>${results.location.street}, ${results.location.city}, ${results.location.state}, ${results.location.postcode}</p>`;
+  //   modalHTML += `<p>${results.dob.date.substring(0.10)}</p></li>`;
+  // }) // end each
+  // modalHTML += '</ul>';
+  // modalClass.innerHTML = modalHTML;
+
+// if (.modal-pic.src === .user-pic.src) then show. Loop through the different modal boxes. is the first modal-box = modal[0]?
+// .modal.children[i]
 
   // Open modal on Click
+  //write of for loop with an if statement to only show the right modal window
 
-usersClass.addEventListener('click',function(e){
-     document.getElementById("mod").style.display = "block";
-});
-
-//write of for loop with an if statement to only show the right modal window
+// usersClass.addEventListener('click',function(e){
+//   for(let i = 0; i >= usersClass.length; i++) {
+//     if
+//      document.getElementById("mod").style.display = "block";
+//    }
+// });
+// $(document).on("click", ".user-box", function(e) {
+//   if ()
+//   $(".modal").show();
+// })
